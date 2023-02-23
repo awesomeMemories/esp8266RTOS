92589cfc8dea9de1c6c4a0104cb78f76efcc3e2a
Matias <matiaswvera@gmail.com>
2023-02-22 22:55:29 -0300

    Use a new port, config. of it and new task to manage a led blinking

--- examples/peripherals/gpio/main/user_main.c
+++ examples/peripherals/gpio/main/user_main.c
@@ -46,6 +46,8 @@ static const char *TAG = "main";
 #define GPIO_INPUT_IO_0     4
 #define GPIO_INPUT_IO_1     5
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define GPIO_OUTPUT_IO_2    12
+#define GPIO_OUTPUT_PIN_LED  (1ULL<<GPIO_OUTPUT_IO_2)
 
 static xQueueHandle gpio_evt_queue = NULL;
 
@@ -63,6 +65,17 @@ static void gpio_task_example(void *arg)
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
             ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
         }
+    }
+}
+
+static void gpio_task_blinker(void *arg)
+{
+    int counter = 0;
+
+    while (1) {
+        ESP_LOGI(TAG, "Led cnt: %d\n", counter++);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(GPIO_OUTPUT_IO_2, counter % 2);
     }
 }
 
@@ -95,10 +108,31 @@ void app_main(void)
     //change gpio intrrupt type for one pin
     gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
+    //BLINKER LED PIN
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO15/16
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_LED;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     //start gpio task
     xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    //                                                               |
+    //                                                               priority
+    //start blinker task
+    xTaskCreate(gpio_task_blinker, "gpio_task_blinker", 1048, NULL, 10, NULL);
+    //                                                               |
+    //                                                               priority
 
     //install gpio isr service
     gpio_install_isr_service(0);
